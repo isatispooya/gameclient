34218d5262fbeb1caa90382061e7e6aabf2619cc
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2024-12-07 18:50:52 +0330

    121212

--- src/mudole/sejam/hooks/useSejamVerify.ts
+++ src/mudole/sejam/hooks/useSejamVerify.ts
@@ -3,7 +3,7 @@ import verifyOtpSejam from "../services/verifyotp";
 import useMissionsGet from "../../missions/hooks/useMissionsGet";
 const useSejamVerify = () => {
   const { refetch } = useMissionsGet();
-  const { mutate: verifyOtpSejamMutate } = useMutation({
+  const { mutate: verifyOtpSejamMutate, isPending } = useMutation({
     mutationKey: ["verifyOtpSejam"],
     mutationFn: (params: { otp: string; uniqueIdentifier: string }) =>
       verifyOtpSejam(params.otp, params.uniqueIdentifier),
@@ -12,7 +12,7 @@ const useSejamVerify = () => {
     },
   });
 
-  return { verifyOtpSejamMutate };
+  return { verifyOtpSejamMutate, isPending };
 };
 
 export default useSejamVerify;
